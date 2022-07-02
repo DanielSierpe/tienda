b044134f15951bf125a61d7431e39fe85450e6ba
DanielSierpe <daniel.sierpe.l@gmail.com>
2022-07-01 23:31:44 -0400

    registro

---
+++ back/controllers/ClienteController.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var Cliente = require('../models/cliente');
+
+const registro_cliente = async function(req,res){
+    //
+    var data = req.body;
+    var reg = await Cliente.create(data);
+    console.log(reg);
+    res.status(200).send({message:reg});
+}
+
+module.exports = {
+    registro_cliente
+}
